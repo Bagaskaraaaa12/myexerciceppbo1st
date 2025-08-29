16952c014cbacf56393836887f064720de51dd0a
bgas_k <h1101241023@student.untan.ac.id>
2025-08-29 15:51:39 +0700

    Update home.php

--- home.php
+++ home.php
@@ -17,33 +17,16 @@
 <p>Pertama aku akan membuat sebuah percobaan untuk menghitung luas persegi panjang</p>
 <?php
 // index.php
+$nama = "Bagaskara"
 $panjang = 5;
 $lebar = 10;
 $hasil = $panjang * $lebar;
 ?>
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <title>Website PHP di Hugging Face</title>
-    <style>
-        body { font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }
-        .container { max-width: 600px; margin: 0 auto; }
-        .box { background: #f0f8ff; padding: 20px; border-radius: 10px; margin-top: 20px; }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h1>Selamat Datang di Website Bagaskara!</h1>
-        <p>pada laman web ini kamu akan diajari tentang bagaimana cara menghitung luas persegi panjang! wahai <strong><?= htmlspecialchars($nama) ?></strong></p>
-        <div class="box">
-        </div>
-    </div>
-      <div>
+    <div>
     Hasil dari perhitungan sebuah persegi panjang dengan panjang 
     <?= $panjang ?> dan lebar <?= $lebar ?> adalah:
-</div>
-<div><strong><?= $hasil ?>
+    </div>
+    <div><strong><?= $hasil ?> <br>
 
 <p> selanjutnya, aku akan membuat sebuah program dimana aku akan menghitung sebuah class produk dengan harga, stok, dan merk yang ada</p>
 <?php
