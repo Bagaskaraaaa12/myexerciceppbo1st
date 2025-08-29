cd8911df054c26e83a9753a584ee0abae80c9f0c
bgas_k <h1101241023@student.untan.ac.id>
2025-08-29 15:07:40 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,22 @@
 <?php
-echo "<h1>Ini halaman HOME</h1>";
-echo "<p>Selamat datang kembali di laman utama!</p>";
-?>
+class produk {
+    // Property
+    public $merk;
+    public $harga;
+    public $stok;
+
+    // Method
+    public function beli() {
+        return "anda membeli barang $this->merk seharga $this->harga yang memiliki sisa stok $this->stok, sekarang sisa stok yang ada adalah $stok-1";
+    }
+}
+
+Membuat Object
+<?php
+// Membuat objek dari class produk
+$merk1 = new produk();
+$merk1->merk = "chitato";
+$merk1->harga = 10000;
+$merk1->stok = 50;
+
+echo $mobil1->jalan(); 
