553c228dba627f41bb7d807d8e273271fba47060
bgas_k <h1101241023@student.untan.ac.id>
2025-08-29 16:08:40 +0700

    Update latihan2.php

--- latihan2.php
+++ latihan2.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Persegi Panjang</title>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+            margin-top: 50px;
+        }
+    </style>
+</head>
+<body>
