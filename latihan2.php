ff662d3c8a921ee8292d00d72b7ffb2f4530da3f
bgas_k <h1101241023@student.untan.ac.id>
2025-08-29 16:10:15 +0700

    Update latihan2.php

--- latihan2.php
+++ latihan2.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Persegi Panjang</title>
+    <title>Produk</title>
     <style>
         body {
             text-align: center;
@@ -11,3 +11,38 @@
     </style>
 </head>
 <body>
+<h1> Halo, disini aku akan membuat sebuah program dimana aku akan menghitung sebuah class produk dengan harga, stok, dan merk yang ada</h1>
+<?php
+class Produk {
+    public $nama;
+    public $harga;
+    public $stok;
+
+    public function tampilkanInfo() {
+        return "Produk: $this->nama dengan Harga: Rp $this->harga, Mempunyai sisa Stok: $this->stok";
+    }
+
+    public function beliProduk($jumlah) {
+        if ($jumlah > $this->stok) {
+            return "Maaf, stok tidak cukup untuk membeli $jumlah item.";
+        } else {
+           $this->stok -= $jumlah;
+            $total = $this->harga * $jumlah;
+            return "Anda berhasil membeli $jumlah $this->nama dengan total harga Rp $total. Sisa stok: $this->stok";
+        }
+    }
+}
+$produk1 = new Produk();
+$produk1->nama = "Chitato";
+$produk1->harga = 10000;
+$produk1->stok = 50;
+echo "<div>";
+echo $produk1->tampilkanInfo();
+echo "<br><br>";
+echo $produk1->beliProduk(3);
+echo "<br><br>";
+echo $produk1->tampilkanInfo();
+echo "</div>";
+?>
+</body>
+</html>
