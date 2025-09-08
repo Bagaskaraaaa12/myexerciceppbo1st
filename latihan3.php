0d8f1aaaf689fc8023d4060d65cc458fcfeaa3fa
Bagaskaraaaa12 <h1101241023@student.untan.ac.id>
2025-09-08 16:12:48 +0700

    upd

--- latihan3.php
+++ latihan3.php
@@ -25,6 +25,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title>Login Sederhana</title>
+    <style>
+        .menu a {
+            display: inline-block;
+            padding: 20px 40px;
+            background: rgba(255, 255, 255, 0.8); /* kotak semi transparan */
+            border-radius: 12px;
+            text-decoration: none;
+            font-weight: bold;
+            color: #333;
+            transition: 0.3s;
+            box-shadow: 0px 4px 10px rgba(0,0,0,0.3);
+        }
+        .menu a:hover {
+            background: linear-gradient(135deg, #9a92e5ff, #ffffffff);
+            color: black;
+            transform: translateY(-5px); 
+            box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.83);
+        }
+    </style>
+    
 </head>
 <body>
     <h1>Form Login</h1>
@@ -35,6 +55,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </form>
 
     <p><?= $result ?></p>
+    <p> jika anda ingin menuju ke menu home tekan tombol dibawah ini </p>
+    <div class="menu">
+        <a href="home.php"> HOME </div>
 </body>
 </html>
 
