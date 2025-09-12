20de636cc9ce4d1f37bf678f7cb9444b952bc166
bgas_k <h1101241023@student.untan.ac.id>
2025-09-12 15:28:49 +0700

    Update enkapsulasi.php

--- enkapsulasi.php
+++ enkapsulasi.php
@@ -61,6 +61,29 @@ echo $mobil_yuda->paksa_hidup(); // "Hidupkan Mobil"
 
 ?>
 
+<br>
+
+<?php
+//Buat class motor
+class motor
+{
+  //property dengan hak akses protected
+  protected $jenis_mesin = "Yamaha CB JKT 48";
+}
+//buat class yamaha
+class yamaha extends motor
+{
+  public function tampilkan_jenismesin()
+  {
+    return $this->jenis_mesin;
+  }
+}
+//buat objek dari class yamaha (instantiation)
+$yamaha_yuda = new yamaha();
+//jalankan method
+echo $yamaha_yuda->tampilkan_jenismesin(); //“Yamaha CB JKT 48”
+?>
+
 <div>
     <p> tekan logo dibawah jika anda ingin kembali ke menu home</p>
     <a href="home.php"> Home </a>
