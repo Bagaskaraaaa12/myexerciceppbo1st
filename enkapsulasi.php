bee25f9fdb66da42e2747e12ab9860b4cab376bb
bgas_k <h1101241023@student.untan.ac.id>
2025-09-12 15:23:46 +0700

    Update enkapsulasi.php

--- enkapsulasi.php
+++ enkapsulasi.php
@@ -29,6 +29,36 @@ echo $mobil_yuda->pemilik; //Syahrukhan
 echo $mobil_yuda->hidupkan_mobil(); //hidupkan mobil
 
 ?>
+
+<?php
+// buat class mobil
+class mobil
+{
+  // buat protected property
+  protected $pemilik = "yuda";
+  public function akses_pemilik()
+  {
+    return $this->pemilik;
+  }
+  protected function hidupkan_mobil()
+  {
+    return "Hidupkan Mobil";
+  }
+  public function paksa_hidup()
+  {
+    return $this->hidupkan_mobil();
+  }
+}
+// buat objek dari class mobil (instansiasi)
+$mobil_yuda = new mobil();
+// jalankan method akses_pemilik()
+echo $mobil_yuda->akses_pemilik(); // "yuda"
+echo "\n";
+// jalankan method paksa_hidup()
+echo $mobil_yuda->paksa_hidup(); // "Hidupkan Mobil"
+
+?>
+
 <div>
     <p> tekan logo dibawah jika anda ingin kembali ke menu home</p>
     <a href="home.php"> Home </a>
