99f17139514dbbacb1196ca75d91bef58b234caf
bgas_k <h1101241023@student.untan.ac.id>
2025-09-19 13:56:45 +0700

    Update this.php

--- this.php
+++ this.php
@@ -1,1 +1,17 @@
+<?php
 
+class Mobil {
+    public $pemilik;
+    public $merk;
+    public $warna;
+
+     public function hidupkan_mobil () {
+        return "hidupkan mobil";
+     }
+     public function matikan_mobil () {
+        return "matikan  mobil";
+     }
+}
+
+$mobilsahroni = new mobiil();
+echo $mobilsahroni 
